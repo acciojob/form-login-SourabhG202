da736add4762b37574ae040f8e34e8c0d61f8fda
Sourabh G <154436409+SourabhG202@users.noreply.github.com>
2025-04-23 14:34:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	event.preventDefault();
+	const fname = document.querySelector('input[name="fname"]').value.trim();
+	const lanem  = document.querySelector('input[name="lanem"]');
+
+	const fullName = `${fname} ${lanem}`;
+	alert(fullName);
+	
 
 }
